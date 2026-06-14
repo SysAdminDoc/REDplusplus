0af23a2a5fc29051d79b4049faf9169a3da784ea
SysAdminDoc <matt_parker@outlook.com>
2026-06-13 23:47:34 -0400

    style: refresh RED++ branding assets
    Use the supplied RED++ folder/search/check artwork for the executable icon, 128px resource, help navigation logo, and README banner.
    
    Update the WPF titlebar to render the higher-resolution logo resource and sync v1.5.17 changelog/package metadata.

--- packaging/chocolatey/tools/chocolateyInstall.ps1
+++ packaging/chocolatey/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop'
 
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $packageName = 'redplusplus.portable'
-$url = 'https://github.com/SysAdminDoc/REDplusplus/releases/download/v1.5.16/RED++_v1.5.16.zip'
+$url = 'https://github.com/SysAdminDoc/REDplusplus/releases/download/v1.5.17/RED++_v1.5.17.zip'
 $checksum = ''
 $checksumType = 'sha256'
 
