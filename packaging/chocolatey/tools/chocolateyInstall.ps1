8b5ac7b3692b2c62fa97a7c8a905eaad6763ddc5
SysAdminDoc <matt_parker@outlook.com>
2026-06-26 10:12:59 -0700

    docs: polish bundled help for v1.6.2

--- packaging/chocolatey/tools/chocolateyInstall.ps1
+++ packaging/chocolatey/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop'
 
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $packageName = 'redplusplus.portable'
-$url = 'https://github.com/SysAdminDoc/REDplusplus/releases/download/v1.6.1/RED++_v1.6.1.zip'
+$url = 'https://github.com/SysAdminDoc/REDplusplus/releases/download/v1.6.2/RED++_v1.6.2.zip'
 $checksum = ''
 $checksumType = 'sha256'
 
